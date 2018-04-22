8568576f325110fa3c9e045c6a943f3f12efa07b
Sumukh Ballal <sumukhballal@gmail.com>
2018-04-23 00:29:29 +0530

    Added angular material and flex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import {  MatToolbarModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import 'hammerjs';
 
 
 import { AppComponent } from './app.component';
 
 
+
 @NgModule({
-  declarations: [
+  declarations:[
     AppComponent
   ],
-  imports: [
-    BrowserModule
+  imports:[
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FlexLayoutModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers:[],
+  bootstrap:[AppComponent]
 })
 export class AppModule { }
